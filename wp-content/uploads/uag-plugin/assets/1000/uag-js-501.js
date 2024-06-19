291aab62e86aa35a3d4b0f1213fbcbe09be293f1
ljsherlock <lewis@ljsherlock.com>
2024-06-19 12:33:24 +0700

    add uploads to test if dploy will deploy it.

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-501.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"30bfd676","reCaptchaEnable":true,"reCaptchaType":"v3","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"lewis@ljsherlock.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"New message from user at questautralia.com.au","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-30bfd676', 501 );
+});
+ });
